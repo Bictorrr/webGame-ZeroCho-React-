230894fe7dc8684ef7546310118e643a5e3ac567
Bictorrr <122665852+Bictorrr@users.noreply.github.com>
2023-03-30 23:00:24 +0900

    Create client.jsx

---
+++ 3.NumberBaseball/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import NumberBaseball from './NumberBaseball2';
+
+ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
