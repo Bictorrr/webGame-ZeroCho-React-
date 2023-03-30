789a64d07f5c951187a4a0558bfb69c70e3e3c70
Bictorrr <122665852+Bictorrr@users.noreply.github.com>
2023-03-30 23:00:49 +0900

    Add files via upload

---
+++ 3.NumberBaseball/NumberBaseball2.jsx
@@ -0,0 +1,87 @@
+import React, { useRef, useState, useCallback } from "react";
+import Try from './Try2';
+
+const getNumbers = () => {
+  const candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (let i = 0; i < 4; i += 1) {
+    const chosen = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+  return array;
+};
+
+const NumberBaseball = () => {
+  const [result, setResult] = useState('')
+  const [value, setValue] = useState('')
+  const [tries, setTries] = useState([])
+  const [answer, setAnswer] = useState(getNumbers)
+  const inputEl = useRef(null);
+
+
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+    if (value === answer.join('')) {
+      setResult('함런');
+      setTries((prevTries) => (
+        [...prevTries, { try: value, result: '함런' }]
+      ));
+      alert('게임을 다시 시작합니다!');
+      setValue('')
+      setAnswer(getNumbers())
+      setTries([])
+      inputEl.current.focus();
+    } else {
+      const answerArray = value.split('').map((v) => parseInt(v))
+      let strike = 0;
+      let ball = 0;
+      if (tries.length >= 9) {
+        setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다`);
+        alert('게임을 다시 시작합니다!')
+        setValue('');
+        setAnswer(getNumbers());
+        setTries([]);
+        inputEl.current.focus();
+      } else {
+        for (let i = 0; i < 4; i += 1) {
+          if (answerArray[i] === answer[i]) {
+            strike += 1;
+          } else if (answer.includes(answerArray[i])) {
+            ball += 1;
+          }
+        }
+        setTries((prevTries) =>
+        (
+          [...prevTries, { try: value, result: `${strike} 스트라이크, ${ball} 볼입니다` }]
+        ))
+
+        setValue('');
+        inputEl.current.focus();
+      }
+    }
+  }, [value, answer]);
+  const onChangeInput = useCallback((e) => setValue(e.target.value), []);
+
+  return (
+    <>
+      <h1>{result}</h1>
+      <form onSubmit={onSubmitForm}>
+        <input
+          maxLength={4}
+          onChange={onChangeInput}
+          ref={inputEl}
+          value={value}
+        />
+        <button>입력!</button>
+      </form>
+      <div>시도: {tries.length}</div>
+      <ul>
+        {tries.map((v, i) => (
+          <Try key={`${i + 1}차 시도 : ${v.try}`} tryInfo={v} />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default NumberBaseball; 
