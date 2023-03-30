d34cccd9860b419d686badf86b1736565f68b877
Bictorrr <122665852+Bictorrr@users.noreply.github.com>
2023-03-30 23:10:08 +0900

    Create client.jsx

---
+++ 8.MineSearch/client.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MineSearch from './MineSearch';
+
+ReactDOM.createRoot(document.querySelector('#root')).render(<MineSearch />)
+
