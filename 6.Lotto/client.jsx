8fb671c269ff93d2c5dfb5485e33cc043f7c8030
Bictorrr <122665852+Bictorrr@users.noreply.github.com>
2023-03-30 23:06:56 +0900

    Create client.jsx

---
+++ 6.Lotto/client.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Lotto from './LottoHooks';
+
+ReactDOM.createRoot(document.querySelector('#root')).render(< Lotto />)
+
+
+
+
+
+
+
+
+
+
